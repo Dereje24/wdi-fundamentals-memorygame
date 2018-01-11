ffb5e4210afd61be83ab4c3ec52fb56e1f5d41d8
Dereje24 <ddilnesaw@yahoo.com>
2018-01-10 22:26:42 -0800

    added some functions and fixed some css

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,24 @@
 
 var cards =["queen", "queen", "king", "king"]
 var cardsInPlay = []
-var cardOne = cards[0]
-cardsInPlay.push(cardOne);
-console.log('user flliped queen');
-var cardTwo = cards[2]
-cardsInPlay.push(cardTwo);
-console.log('user flliped king');
+var checkForMatch = function(){
+if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+};
+var flipCard = function (cardID){
+	checkForMatch();
 if (cardsInPlay.length){
 	cardsInPlay === 2;
 	 if (cardsInPlay[0] === cardsInPlay[1]){
 		alert('You found a match');
 	}
 	else alert('Sorry, try again');
+	};
+	console.log("user flliped " + " " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
 };
+flipCard(0);
+flipCard(2);
