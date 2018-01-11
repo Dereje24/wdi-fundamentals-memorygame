29ba50a1e28337c5469d6fa9242c6fdd4993b2b3
Dereje24 <ddilnesaw@yahoo.com>
2018-01-10 18:38:51 -0800

    added game logic and some if ... else statments

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,16 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
+
+var cards =["queen", "queen", "king", "king"]
+var cardsInPlay = []
+var cardOne = cards[0]
+cardsInPlay.push(cardOne);
+console.log('user flliped queen');
+var cardTwo = cards[2]
+cardsInPlay.push(cardTwo);
+console.log('user flliped king');
+if (cardsInPlay.length){
+	cardsInPlay === 2;
+	 if (cardsInPlay[0] === cardsInPlay[1]){
+		alert('You found a match');
+	}
+	else alert('Sorry, try again');
+};
