743156db6ffa9b8e09285d32741999c4747e1d62
Dereje24 <ddilnesaw@yahoo.com>
2018-01-11 00:00:26 -0800

    added objects and more cosnosle logs

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,28 @@
 
-var cards =["queen", "queen", "king", "king"]
+var cards =[
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png" 
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+
+];
+
 var cardsInPlay = []
 var checkForMatch = function(){
 if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -17,8 +40,12 @@ if (cardsInPlay.length){
 	}
 	else alert('Sorry, try again');
 	};
-	console.log("user flliped " + " " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log("user flliped " + " " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+	
 };
+console.log(cards.cardImage);
+console.log(cards.suit);
+
 flipCard(0);
 flipCard(2);
